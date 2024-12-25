83be17a7ff25ba0d1f70f7946577da14adaec6a8
DataWizVerma <180633049+DataWizVerma@users.noreply.github.com>
2024-12-25 14:43:47 +0530

    ok

---
+++ 01.py
@@ -0,0 +1,14 @@
+import pandas as pd
+data={
+    'Id':["S01","S02","S03","S04","S05"],
+    'Student':["Amit","John","Jacob","David","Steve"],
+    'Rank':[1,2,3,5,2],
+    'Marks':[95,70,80,60,90]
+
+}
+
+dataFrame = pd.DataFrame(data)
+print("Student Record \n,datFrame")
+
+dataFrame.insert(2,"Roll no", [101,102,103,104,105])
+print(dataFrame)
